085b4f87c4a6312ced386a851d30c4cd8b19ae8b
Rodriguezlui002 <58277521+Rodriguezlui002@users.noreply.github.com>
2021-10-14 10:48:21 -0700

    General layout for calculator.

--- vue-calculator/src/App.vue
+++ vue-calculator/src/App.vue
@@ -1,21 +1,52 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="min-w-full w-screen min-h-full h-screen flex justify-center place-items-center">
+    <div class="grid grid-cols-1 w-96 h-80 p-6 bg-red-900">
+      <div class="text-xl font-medium text-center bg-blue-400 whitespace-nowrap overflow-auto">solution</div>
+
+      <div class="grid grid-cols-5">
+        <div class="bg-purple-400">&#94;</div>
+        <div class="bg-purple-400">Sin</div>
+        <div class="bg-purple-400">Cos</div>
+        <div class="bg-purple-400">Tan</div>
+        <div class="bg-purple-400">Cot</div>
+        <div class="bg-purple-400">ArcSin</div>
+        <div class="bg-purple-400">ArcCos</div>
+        <div class="bg-purple-400">ArcTan</div>
+        <div class="bg-purple-400">ArcCtg</div>
+        <div class="bg-purple-400">Ln</div>
+        <div class="bg-purple-400">log10</div>
+        <div class="bg-purple-400">[]</div>
+        <div class="bg-purple-400">{}</div>
+        <div class="bg-purple-400">()</div>
+      </div>
+
+      <div class="grid grid-cols-2 gap-3 text-center bg-green-300">
+        <div class="grid grid-cols-3">
+          <div class="flex flex-col justify-center bg-green-500">1</div>
+          <div class="flex flex-col justify-center bg-green-500">2</div>
+          <div class="flex flex-col justify-center bg-green-500">3</div>
+          <div class="flex flex-col justify-center bg-green-500">4</div>
+          <div class="flex flex-col justify-center bg-green-500">5</div>
+          <div class="flex flex-col justify-center bg-green-500">6</div>
+          <div class="flex flex-col justify-center bg-green-500">7</div>
+          <div class="flex flex-col justify-center bg-green-500">8</div>
+          <div class="flex flex-col justify-center bg-green-500">9</div>
+          <div class="flex flex-col justify-center bg-green-500">0</div>
+          <div class="flex flex-col justify-center bg-green-500">.</div>
+          <div class="flex flex-col justify-center bg-green-500">&#x0186;</div>
+        </div>
+
+        <div class="grid">
+          <div class="bg-yellow-400">&#247;</div>
+          <div class="bg-yellow-400">&#215;</div>
+          <div class="bg-yellow-400">&#8722;</div>
+          <div class="bg-yellow-400">&#43;</div>
+          <div class="bg-yellow-400">&#61;</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+</script>
