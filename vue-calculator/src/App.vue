a9843a5bb6d5f975b33a76f5f61fe1063d934772
Rodriguezlui002 <58277521+Rodriguezlui002@users.noreply.github.com>
2021-11-01 16:46:00 -0700

    Connected logic to UI.

--- vue-calculator/src/App.vue
+++ vue-calculator/src/App.vue
@@ -1,50 +1,53 @@
 <template>
   <div class="min-w-full w-screen min-h-full h-screen flex justify-center place-items-center">
-    <div class="grid grid-cols-1 w-96 pt-9 bg-gray-800">
+    <div class="grid grid-cols-1 w-96 pt-9 bg-gray-700">
 
-      <div class="h-16 pr-4 flex justify-end items-center text-3xl text-ivory whitespace-nowrap overflow-auto">solution</div>
+      <div class="h-16 pr-4 flex justify-end items-center text-3xl text-ivory whitespace-nowrap overflow-auto">{{ output }}</div>
 
       <div class="h-96 bg-gray-900 flex flex-col p-4 rounded-t-2xl">
         <div class="h-36 grid grid-cols-4 gap-1 mb-4">
-          <button class="bg-cwu-red text-ivory rounded text-lg">&#94;</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Sin</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Sin</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Cos</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Tan</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Cot</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">ArcSin</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">ArcCos</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">ArcTan</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">ArcCtg</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">Ln</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">log10</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">[]</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">{}</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">()</button>
-          <button class="bg-cwu-red text-ivory rounded text-lg">&#247;</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('^')">&#94;</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('sin(')">Sin</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('cos(')">Cos</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('tan(')">Tan</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('cot(')">Cot</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('arcsin(')">ArcSin</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('arccos(')">ArcCos</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('arctan(')">ArcTan</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('arcctg(')">ArcCtg</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('ln(')">Ln</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('log(')">Log</button>
+          <button class="bg-cwu-red text-ivory rounded text-lg" @click="appendExpression('sqrt(')">&#8730;</button>
         </div>
 
         <div class="flex-1 flex gap-1">
           <div class="flex-1 grid grid-cols-3 gap-1 text-center">
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">1</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">2</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">3</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">4</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">5</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">6</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">7</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">8</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">9</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">0</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">.</button>
-            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl">&#x0186;</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('1')">1</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('2')">2</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('3')">3</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('4')">4</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('5')">5</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('6')">6</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('7')">7</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('8')">8</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('9')">9</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('0')">0</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="appendExpression('.')">.</button>
+            <button class="bg-ivory text-gray-800 rounded-md font-semibold text-xl" @click="clearOut()">&#x0186;</button>
           </div>
 
-          <div class="w-16 grid gap-1">
-            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl">&#215;</button>
-            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl">&#8722;</button>
-            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl">&#43;</button>
-            <button class="bg-red-orange text-ivory rounded-md font-semibold text-xl">&#61;</button>
+          <div class="w-24 grid grid-cols-2 gap-1">
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('[')">[</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression(']')">]</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('{')">{</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('}')">}</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('(')">(</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression(')')">)</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('*')">&#215;</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('-')">&#8722;</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('+')">&#43;</button>
+            <button class="bg-blue-ncs text-ivory rounded-md font-semibold text-xl" @click="appendExpression('/')">&#247;</button>
+            <button class="col-span-2 bg-red-orange text-ivory rounded-md font-semibold text-xl" @click="solution">&#61;</button>
           </div>
         </div>
       </div>
@@ -53,4 +56,31 @@
 </template>
 
 <script>
+import { rpn, parse, evalu } from './calcLogic.js';
+
+export default {
+  data() {
+    return {
+      output: ''
+    }
+  },
+  methods: {
+    appendExpression(char) {
+      this.output = this.output + char;
+    },
+    clearOut() {
+      this.output = '';
+    },
+    solution() {
+      try {
+        let arr = rpn(this.output);
+        let tree = parse(arr);
+        this.output = evalu(tree);
+      } catch (e) {
+        console.log(e);
+        this.output = 'error';
+      }
+    }
+  }
+}
 </script>
---
+++ vue-calculator/src/calcLogic.js
@@ -0,0 +1,247 @@
+let type_op = 'o';
+let type_const = 'c';
+let type_func = 'f';
+let type_else = 'e';
+let type_leftpara = '(';
+let type_rightpara = ')';
+
+function genFunc(evalu, type = type_func, prec = 0, left = true) {
+    return {
+        evalu: evalu,
+        t: type,
+        prec: prec,
+        left: left
+    }
+}
+
+function Node(val, func = true, unary = true) {
+    return {
+        val: val,
+        func: func,
+        unary: unary,
+        right: null,
+        left: null,
+        name: ''
+    }
+}
+
+let UNARY_FUNCS = {
+    sin: genFunc((x) => Math.sin(x)),
+    cos: genFunc((x) => Math.cos(x)),
+    tan: genFunc((x) => Math.tan(x)),
+    cot: genFunc((x) => 1 / Math.tan(x)),
+    arcsin: genFunc((x) => Math.asin(x)),
+    arccos: genFunc((x) => Math.acos(x)),
+    arctan: genFunc((x) => Math.atan(x)),
+    arcctg: genFunc((x) => Math.PI / 2 - Math.atan(x)),
+    ln: genFunc((x) => Math.log(x)),
+    log: genFunc((x) => Math.log10(x)),
+    sqrt: genFunc((x) => Math.sqrt(x))
+};
+
+let BINARY_FUNCS = {
+    '+': genFunc((x, y) => x + y, type_op, 2),
+    '-': genFunc((x, y) => x - y, type_op, 2),
+    '*': genFunc((x, y) => x * y, type_op, 3),
+    '/': genFunc((x, y) => x / y, type_op, 3),
+    '^': genFunc((x, y) => Math.pow(x, y), type_op, 4, false),
+}
+
+let functions = Object.keys(UNARY_FUNCS).concat(Object.keys(BINARY_FUNCS));
+let operators = '+-*/^';
+let left_para = '({[';
+let right_para = ')}]';
+
+function isNumber(x) {
+    if (typeof x === 'number') {
+        return true;
+    }
+
+    return !isNaN(x) || x === '.';
+}
+
+function getNumVal(x) {
+    if (typeof x === 'number') {
+        return x;
+    } else {
+        return parseFloat(x);
+    }
+}
+
+function findElement(i, eqn, list) {
+    for (let k = 0; k < list.length; k++) {
+        let n = list[k].length;
+        if (eqn.substring(i, i + n) === list[k]) {
+            return [true, list[k], n];
+        }
+    }
+
+    return [false, '', 1];
+}
+
+function getPrecedence(op) {
+    if (Object.keys(BINARY_FUNCS).includes(op)) {
+        return BINARY_FUNCS[op].prec;
+    }
+
+    return 0;
+}
+
+function isLeftAssociative(op) {
+    if (Object.keys(BINARY_FUNCS).includes(op)) {
+        return BINARY_FUNCS[op].left;
+    }
+
+    return true;
+}
+
+function rpn(eqn) {
+    let queue = [];
+    let stack = [];
+
+    let obj = '';
+    let type = '';
+
+    // Evaluate each token
+    for (let i = 0; i < eqn.length; i++) {
+        let token = eqn[i];
+
+        if (token === ' ' || token === ',') {
+            continue;
+        }
+
+        // Determine if the toke is a number, function, or operator
+        if (isNumber(token)) {
+            type = type_const;
+
+            obj = token;
+            if (i < eqn.length - 1) {
+                while (isNumber(eqn[i + 1])) {
+                    obj += eqn[i + 1];
+                    i++;
+                    if (i >= eqn.length - 1) {
+                        break;
+                    }
+                }
+            }
+
+            obj = getNumVal(obj);
+        } else {
+            let data = findElement(i, eqn, functions);
+            let found = data[0];
+            obj = data[1];
+            let len = data[2];
+            if (found) {
+                type = operators.includes(obj) ? type_op : type_func;
+            } else {
+                if (left_para.includes(token)) {
+                    type = type_leftpara;
+                } else if (right_para.includes(token)) {
+                    type = type_rightpara;
+                } else {
+                    type = type_else;
+                }
+            }
+            i += len - 1;
+        }
+
+        let last_stack = stack[stack.length - 1];
+        switch (type) {
+            case type_const:
+                queue.push(obj);
+                break;
+            case type_func:
+                stack.push(obj);
+                break;
+            case type_op:
+                if (stack.length != 0) {
+                    while (((functions.includes(last_stack) && !operators.includes(last_stack)) || getPrecedence(last_stack) > getPrecedence(obj) || (getPrecedence(last_stack) === getPrecedence(obj) && isLeftAssociative(last_stack))) && !left_para.includes(last_stack)) {
+                        queue.push(stack.pop());
+                        if (stack.length === 0) {
+                            break;
+                        }
+                        last_stack = stack[stack.length - 1];
+                    }
+                }
+                stack.push(obj);
+                break;
+            case type_leftpara:
+                stack.push('(');
+                break;
+            case type_rightpara:
+                while (last_stack !== '(') {
+                    queue.push(stack.pop());
+                    last_stack = stack[stack.length - 1];
+                }
+                stack.pop();
+                break;
+            default:
+                return null;
+        }
+    }
+
+    while (stack.length > 0) {
+        queue.push(stack.pop());
+    }
+
+    return queue;
+};
+
+function parse(rpn) {
+    let stack = [];
+
+    Array.from(rpn).forEach((token) => {
+        let tree = null;
+
+        if (isNumber(token)) {
+            tree = Node(token, false);
+        } else {
+            if (Object.keys(BINARY_FUNCS).includes(token)) {
+                tree = Node(BINARY_FUNCS[token], true, false);
+
+                let a = stack.pop();
+                let b = stack.pop();
+
+                if (typeof a === 'number') {
+                    tree.right = Node(a, false);
+                } else {
+                    tree.right = a;
+                }
+
+                if (typeof b === 'number') {
+                    tree.left = Node(b, false);
+                } else {
+                    tree.left = b;
+                }
+            } else if (Object.keys(UNARY_FUNCS).includes(token)) {
+                tree = Node(UNARY_FUNCS[token]);
+
+                let a = stack.pop();
+
+                if (typeof a === 'number') {
+                    tree.left = Node(a, false);
+                } else {
+                    tree.left = a;
+                }
+            }
+        }
+        tree.name = token;
+        stack.push(tree);
+    });
+
+    return stack.pop();
+};
+
+function evalu(tree) {
+	if (tree.func) {
+		if (tree.unary) {
+			return tree.val.evalu(evalu(tree.left));
+		} else {
+			return tree.val.evalu(evalu(tree.left), evalu(tree.right));
+		}
+	} else {
+        return tree.val;
+	}
+}
+
+export {rpn, parse, evalu};
